44c3a08ae8860153f71177c7ba9703cc8f72415a
Begaiym <begaiym.akylova@gmail.com>
2021-03-07 00:18:26 +0600

    Add styles for buttons and fix scrolling of slider

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 	let position = 0;
-	const slidesToShow = 5;
-	const slidesToScroll = 2;
+	let itemMargin = 25 * 2;
+	const slidesToShow = 4;
+	const slidesToScroll = 3;
 	const container = $('.slider__container');
 	const track = $('.slider__track');
 	const item = $('.slider__item');
 	const btnPrev = $('.slider__button-prev');
 	const btnNext = $('.slider__button-next');
 	const itemsCount = item.length;
-	const itemWidth = container.width() / slidesToShow;
-	const movePosition = slidesToScroll * itemWidth;
-
-	
+	const itemWidth = (container.width() - (slidesToShow * itemMargin)) / slidesToShow;
+	const itemWithMargin = itemWidth + itemMargin;
+	const movePosition = slidesToScroll * itemWithMargin;
 
 	item.each(function(index, item) {
 		$(item).css({
@@ -20,14 +20,14 @@ $(document).ready(function() {
 	});
 
 	btnPrev.click(function(){
-		const itemsLeft = Math.abs(position) / itemWidth;
-		position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+		const itemsLeft = Math.abs(position) / itemWithMargin;
+		position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWithMargin;
 		setPosition();
 		checkBtns();
 	})
 	btnNext.click(function(){
-		const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
-		position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+		const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWithMargin) / itemWithMargin;
+		position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWithMargin;
 		setPosition();
 		checkBtns();
 	})
@@ -44,6 +44,5 @@ $(document).ready(function() {
 	}
 
 	checkBtns();
-
 });
 
